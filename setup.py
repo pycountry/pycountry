6f6563a41147ecffe4e56f6d4a025e69cfbe05b6
Christian Theune <ct@flyingcircus.io>
2008-05-20 14:00:26 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='0.9dev',
+    version='0.10dev',
     author='Christian Theune',
     author_email='ct@gocept.com',
     description='ISO country, subdivision, language, currency and script '
