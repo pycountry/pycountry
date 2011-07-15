e5e7ad647d49f8025f389cf6796917a779408bce
ctheune <ctheune@localhost>
2011-07-15 08:53:47 +0000

    Back to development: 0.14.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='0.14.1',
+    version='0.14.2dev',
     author='Christian Theune',
     author_email='ct@gocept.com',
     description='ISO country, subdivision, language, currency and script '
