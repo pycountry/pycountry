4cdae9db6aab186dbd9c7738f80c14e6af9bf9ca
Christian Theune <ct@gocept.com>
2013-08-02 08:29:58 +0200

    Back to development: 0.19

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='0.18',
+    version='0.19.dev0',
     author='Christian Theune',
     author_email='ct@gocept.com',
     description='ISO country, subdivision, language, currency and script '
