6cad979ea6cc022b464820d6bcf8fa06cf25f43f
Christian Theune <ct@flyingcircus.io>
2014-02-07 08:19:35 +0100

    Back to development: 1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='1.4',
+    version='1.5.dev0',
     author='Christian Theune',
     author_email='ct@gocept.com',
     description='ISO country, subdivision, language, currency and script '
