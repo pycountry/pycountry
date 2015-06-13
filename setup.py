7fc061542156943adaf49ae64216f8fceac4065d
Christian Theune <ct@flyingcircus.io>
2015-06-13 10:08:29 +0200

    Preparing release 1.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='1.11.dev0',
+    version='1.11',
     author='Christian Theune',
     author_email='ct@gocept.com',
     description='ISO country, subdivision, language, currency and script '
