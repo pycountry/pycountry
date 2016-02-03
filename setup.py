a6eb1e2d225d353508a1136fe2fd303238473490
Christian Theune <ct@flyingcircus.io>
2016-02-03 11:03:04 +0100

    Preparing release 1.20

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='1.20.dev0',
+    version='1.20',
     author='Christian Theune',
     author_email='ct@flyingcircus.io',
     description='ISO country, subdivision, language, currency and script '
