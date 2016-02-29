5eb5ce887c6240c19f76f07cb4b93a0bbe0bebcf
Kevin Deldycke <kevin@deldycke.com>
2016-02-29 10:10:07 +0000

    Add Trove classifiers.

--- setup.py
+++ setup.py
@@ -19,6 +19,25 @@ setup(
     license='LGPL 2.1',
     keywords='country subdivision language currency iso 3166 639 4217 '
              '15924 3166-2',
+    classifiers=[
+        # See: https://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: '        
+        'GNU Lesser General Public License v2 (LGPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Localization',
+    ],
     zip_safe=False,
     packages=find_packages('src'),
     include_package_data=True,
