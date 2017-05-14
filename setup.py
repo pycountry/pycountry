ea894b56b1ee4744f15c0148ab343ae03b5c9ae0
Christian Theune <ct@flyingcircus.io>
2017-05-14 10:37:30 +0200

    Preparing release 17.5.14

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='17.05.14.dev0',
+    version='17.5.14',
     author='Christian Theune',
     author_email='ct@flyingcircus.io',
     description='ISO country, subdivision, language, currency and script '
