3759ec523223f38c009e9951c4486d518efb5417
Christian Theune <ct@flyingcircus.io>
2017-09-23 17:07:07 +0200

    Back to development: 17.9.24

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycountry',
-    version='17.9.23',
+    version='17.9.24.dev0',
     author='Christian Theune',
     author_email='ct@flyingcircus.io',
     description='ISO country, subdivision, language, currency and script '
