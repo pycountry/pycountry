cfaff2bfe2804b6d54ac5a78f8cd51f80dbec867
Christian Theune <ct@flyingcircus.io>
2022-03-05 11:13:42 +0100

    modernize, prepare for python 3.10 as main line
    remove pytest flake8 plugin in favor of pre-commit hooks
    and due to version variability/conflicts over python versions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
+version=${1:-3}
 rm -rf bin develop-eggs eggs include lib parts
-python3 -m venv .
-bin/pip install zc.buildout==2.13.3
+python${version} -m venv .
+bin/pip install zc.buildout==2.13.6
 bin/buildout
