81861025a5e0c89cefd124f080c1d6bf7356a759
Christian Theune <ct@flyingcircus.io>
2022-03-06 10:04:27 +0100

    codestyle after merging older PR

--- src/pycountry/__init__.py
+++ src/pycountry/__init__.py
@@ -11,13 +11,15 @@ try:
 
     resource_filename = pkg_resources.resource_filename
 except ImportError:
+
     def resource_filename(package_or_requirement, resource_name):
         return os.path.join(os.path.dirname(__file__), resource_name)
+
 else:
     try:
         __version__ = pkg_resources.get_distribution("pycountry").version
     except pkg_resources.DistributionNotFound:
-        __version__ = 'n/a'
+        __version__ = "n/a"
 
 
 LOCALES_DIR = resource_filename("pycountry", "locales")
